b219b8e1cbf3bdae1c197819cdba39744236183a
Tole <manutol@gmail.com>
2026-08-15 18:36:30 +0200

    feat(infra): configure S3 backend with DynamoDB locking
    Add backend "s3" block to versions.tf:
    - bucket: toleflaco-terraform-state-2026
    - key: envs/dev/terraform.tfstate
    - dynamodb_table: terraform-state-lock
    - encrypt: true
    
    DynamoDB locking will be migrated to use_lockfile in next session
    (dynamodb_table is deprecated since Terraform 1.11).
    
    ---
    
    Configura backend S3 en versions.tf:
    - bucket: toleflaco-terraform-state-2026
    - key: envs/dev/terraform.tfstate
    - dynamodb_table: terraform-state-lock
    - encrypt: true
    
    DynamoDB locking se migrará a use_lockfile en la siguiente sesión
    (dynamodb_table está deprecated desde Terraform 1.11).

--- infra/versions.tf
+++ infra/versions.tf
@@ -9,4 +9,11 @@ terraform {
       version = "~> 6.58"
     }
   }
+  backend "s3" {
+    bucket         = "toleflaco-terraform-state-2026"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
